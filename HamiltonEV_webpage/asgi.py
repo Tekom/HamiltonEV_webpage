94cf6a39791148bc35c2e707175c46ce48298f16
Felipe <est.felipe.aponte@unimilitar.edu.co>
2023-08-06 20:32:07 -0500

    Fixing errors

--- HamiltonEV_webpage/asgi.py
+++ HamiltonEV_webpage/asgi.py
@@ -4,20 +4,13 @@ ASGI config for HamiltonEV_webpage project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from telemetria.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HamiltonEV_webpage.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns)),
-})
-
+application = get_asgi_application()
