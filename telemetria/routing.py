48c7701908755bb1ff63afba7d894797c29b62b1
Felipe <est.felipe.aponte@unimilitar.edu.co>
2023-08-06 20:26:49 -0500

    Fixing errors

--- telemetria/routing.py
+++ telemetria/routing.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .dinamics import GraphDinamics  
+# from django.urls import path
+# from .dinamics import GraphDinamics  
 
-ws_urlpatterns = [
-    path('ws/telemetria/', GraphDinamics.as_asgi()),
-]
+# ws_urlpatterns = [
+#     path('ws/telemetria/', GraphDinamics.as_asgi()),
+# ]
