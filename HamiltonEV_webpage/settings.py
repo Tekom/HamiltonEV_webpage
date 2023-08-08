752be6cb5de40694e8321db71931f36d33dfc43c
Felipe <est.felipe.aponte@unimilitar.edu.co>
2023-08-07 19:54:07 -0500

    Fixing graphs

--- HamiltonEV_webpage/settings.py
+++ HamiltonEV_webpage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-16s4w=qjs*@r$!k&20fe(hs27v_a%b@%a_-a@00wtat7o70f_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
