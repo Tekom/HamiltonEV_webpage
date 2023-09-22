85868351c24c2e29d05729f99068e6a04126d925
TekCode <89738509+Tekom@users.noreply.github.com>
2023-09-22 01:16:22 -0500

    Delete telemetria directory

--- telemetria/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('sse/', views.sse, name='sse'),
-    path('logout/', views.logout_view, name='logout'),
-    path('download_file/<str:filename>', views.download_file, name='download_file')
-]
